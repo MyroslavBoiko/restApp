8105c335dba94325dbc646d273043a1f3a3ce97f
Myroslav_Boiko <miroslav.boiko228@gmail.com>
2018-11-15 17:35:50 +0200

    refactored mapping of controller

--- src/main/java/com/rest/app/controller/MyController.java
+++ src/main/java/com/rest/app/controller/MyController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController(value = "/teams")
+@RestController
+@RequestMapping("/teams")
 public class MyController {
 
     @Autowired
